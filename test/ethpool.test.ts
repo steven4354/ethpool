a8ae16bf0e7e00d2b402797b6fb857d25c1c543d
steven4354 <steven4354@gmail.com>
2022-07-30 16:09:33 -0700

    test: test add reward

--- test/ethpool.test.ts
+++ test/ethpool.test.ts
@@ -2,12 +2,13 @@ import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import '@nomiclabs/hardhat-ethers'
 
-import { EthPool__factory, EthPool } from '../build/types'
+import { EthPool__factory, EthPool, RewardToken__factory, RewardToken } from '../build/types'
 
 const { getContractFactory, getSigners } = ethers
 
 describe('EthPool', () => {
   let ethPool: EthPool
+  let rewardToken: RewardToken;
 
   beforeEach(async () => {
     // 1
@@ -21,13 +22,54 @@ describe('EthPool', () => {
 
     // 3
     expect(owner).to.eq(signers[0].address)
+
+    // 4
+    const rewardTokenFactory = (await ethers.getContractFactory(
+      "RewardToken", signers[0]
+    )) as RewardToken__factory;
+    const totalSupply = (10 ** 9).toString()
+    rewardToken = await rewardTokenFactory.deploy(
+      ethers.utils.parseEther(totalSupply),
+    )
+
+    // 5
+    const initSupply = await rewardToken.balanceOf(signers[0].address)
+    expect(initSupply.toString()).to.eq('1000000000000000000000000000')
+
+    // 6 set reward
+    await ethPool.setRewardToken(rewardToken.address)
+    const rewardAddr = await ethPool.rewardAddress()
+    expect(rewardToken.address).to.eq(rewardAddr)
   })
 
-  describe('count up', async () => {
-    it('should count up', async () => {
-      // await counter.countUp()
-      // const count = await counter.getCount()
-      // expect(count).to.eq(1)
+  // 7
+  describe('add reward', async () => {
+    it('should succeed', async () => {
+      const depositAmt = 100;
+      await rewardToken.approve(ethPool.address, depositAmt)
+      await ethPool.addReward(depositAmt)
+
+      const ethPoolRewardBal = await rewardToken.balanceOf(ethPool.address)
+      expect(ethPoolRewardBal.toString(), depositAmt.toString())
+    })
+
+    it('should only allow owner to add', async () => {
+      const depositAmt = 100;
+      const signers = await getSigners()
+      const signer1 = signers[1]
+      const signer1Addr = signers[1].address
+      
+      // send some token to signer1
+      rewardToken.transfer(signer1Addr, depositAmt)
+      const signer1TokenBal = await rewardToken.balanceOf(signer1Addr)
+      expect(signer1TokenBal, depositAmt.toString())
+
+      const ethPoolAsSigner1 = await ethPool.connect(signer1)
+      const rewardTokenAsSigner1 = await rewardToken.connect(signer1)
+      await rewardTokenAsSigner1.approve(ethPoolAsSigner1.address, 100)
+
+      // @ts-ignore TODO: see why reverted isn't found by typescript
+      await expect(ethPoolAsSigner1.addReward(100)).to.be.reverted;
     })
   })
 
