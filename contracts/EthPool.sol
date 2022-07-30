b96784951b606c59246d1ec90731119b1d359d47
steven4354 <steven4354@gmail.com>
2022-07-30 16:00:22 -0700

    feat: reward logic

--- contracts/EthPool.sol
+++ contracts/EthPool.sol
@@ -1,13 +1,37 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.7;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// TODO: compile and test
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract EthPool is Ownable {
+    using SafeMath for uint256;
+
+    //Reward Token
+    address public rewardAddress;
+    IERC20 rewardToken;
+
+    uint256 currentReward = 0;
+
     constructor() {
         transferOwnership(tx.origin);
+    }
+
+    /*
+    Set Reward Token Contract
+    */
+    function setRewardToken(address _reward) public onlyOwner {
+        rewardAddress = _reward;
+        rewardToken = IERC20(rewardAddress);
+    }
+
+    /*
+    Add Reward
+    */
+    function addReward(uint256 _rewardAmount) public onlyOwner {
+        //Transfer REWARD
+        currentReward = currentReward.add(_rewardAmount);
+        rewardToken.transferFrom(msg.sender, address(this), _rewardAmount);
     }
 }
